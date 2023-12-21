4666106b8f90a166b790642a965a93a890428864
Joshua Eaton <joshuaeaton@Joshuas-MacBook-Air.local>
2023-12-21 08:44:18 +0000

    add: Added user-info command slash command

---
+++ src/slashCommands/Community/userInfo.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder() 
+    .setName('user-info')
+    .setDescription('Gets the info of a given user.')
+    .addUserOption(option => option.setName('user').setDescription('The user you want the info of.').setRequired(false)),
+    async execute(interaction) {
+
+        const { options } = interaction;
+        
+        const userID = interaction.user.id;
+        const { guild } = interaction;
+
+        const icon1 = guild.iconURL;
+        const roles = guild.emojis.cache.size;
+        const emojis = guild.emojis.cache.size;
+        const id = guild.id;
+
+        const user = options.getUser('user') || interaction.user;
+        const member = await interaction.guild.members.fetch(user.id);
+        const icon = user.displayAvatarURL();
+        const tag = user.tag;
+        const { name } = guild;
+        const banner = await ( await interaction.client.users.fetch(user.id, { force: true})).bannerURL({ size: 4096 });
+        const owner = config.owner
+
+        const userbed = new EmbedBuilder()
+        .setColor("Random")
+        .setAuthor({ name: tag, iconURL: icon })
+        .setThumbnail(icon)
+        .addFields({ name: "Name of Server", value: `${name}`, inline: false })
+        .addFields({ name: "Member", value: `${user}`, inline: true })
+        .addFields({ name: "Roles", value: `${member.roles.cache.map(r => r).join(' ')}`, inline: false })
+        .addFields({ name: "Joined Server", value: `<t:${parseInt(member.joinedAt / 1000)}:R>`, inline: false })
+        .addFields({ name: "Joined Discord", value: `<t:${parseInt(user.createdAt / 1000)}:R>`, inline: false })
+        .addFields({ name: "User ID", value: `${user.id}`, inline: false })
+        .setFooter({ text: `User info tracker | By ${owner}`})
+        .setTimestamp()
+        .setImage(banner)
+
+        await interaction.reply({ embeds: [userbed]});
+    }
+}
